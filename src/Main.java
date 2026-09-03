48cc1461204aec182f33a37577c1d85c1a3d6c0b
077415 <3477430157@qq.com>
2026-09-03 10:59:23 +0800

    0.1窗口

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main{
+    public static void main(String[]args){
+        JFrame frame  = new JFrame("new game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800,600);
+
+        frame.setVisible(true);
+    }
+}
