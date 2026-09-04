d01eba5ca0f564e6f0539aa37bdbce2c4f539a15
077415 <3477430157@qq.com>
2026-09-04 21:19:35 +0800

    0.9加入了爆炸特效

--- src/Bown.java
+++ src/Bown.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
@@ -7,7 +8,7 @@ public class Bown {
     int x;
     int y;
     int step = 0;
-    Image image;
+    BufferedImage image;
 
 
     protected void draw(Graphics g) {
@@ -20,15 +21,44 @@ public class Bown {
     public Bown(int x, int y) {
         this.x = x;
         this.y = y;
+
         try {
-            image = ImageIO.read(new File("Gemini_Generated_Image_y59d3fy59d3fy59d.jpeg"));
+            image = ImageIO.read(
+                    new File("Gemini_Generated_Image_y59d3fy59d3fy59d.jpeg")
+            );
+            makeTransparent();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(image);
     }
     public void step(){
         step ++;
+    }
+    private void makeTransparent() {
+        BufferedImage newImage = new BufferedImage(
+                image.getWidth(),
+                image.getHeight(),
+                BufferedImage.TYPE_INT_ARGB
+        );
+
+        for (int x = 0; x < image.getWidth(); x++) {
+            for (int y = 0; y < image.getHeight(); y++) {
+
+                int rgb = image.getRGB(x, y);
+
+                int red = (rgb >> 16) & 0xff;
+                int green = (rgb >> 8) & 0xff;
+                int blue = rgb & 0xff;
+
+                if (red < 30 && green < 30 && blue < 30) {
+                    newImage.setRGB(x, y, 0x00000000);
+                } else {
+                    newImage.setRGB(x, y, rgb);
+                }
+            }
+        }
+
+        image = newImage;
     }
 
 
